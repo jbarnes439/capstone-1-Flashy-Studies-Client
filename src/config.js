47f6afb81d7da19bc340c5dd3f2eacc9c4dafb0d
Josh Barnes <JBarnes49@gmail.com>
2020-10-09 14:17:32 -0700

    api url changed

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 export default {
-  API_ENDPOINT: 'postgres://ovcuzsrvevaymo:1986e4e8b646a878e64a78a4b8eb65f87333cb1685b4adf3abc70574b5e73d1b@ec2-54-160-120-28.compute-1.amazonaws.com:5432/d17ta7hp05u41v',
+  API_ENDPOINT: 'https://pure-sea-96268.herokuapp.com/api',
   TOKEN_KEY: 'auth-token-goes-here',
   USERNAME: 'current-user'
 }
 
 // API_ENDPOINT: 'http://localhost:2020/api', <-- for development
+// postgres://ovcuzsrvevaymo:1986e4e8b646a878e64a78a4b8eb65f87333cb1685b4adf3abc70574b5e73d1b@ec2-54-160-120-28.compute-1.amazonaws.com:5432/d17ta7hp05u41v
