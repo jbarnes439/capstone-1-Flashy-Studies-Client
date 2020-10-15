eb1690a3c209ca117c4b8262c9b145b17b2ad644
Josh Barnes <JBarnes49@gmail.com>
2020-10-14 19:44:11 -0700

    changed API endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:2020/api',
+  API_ENDPOINT: 'https://pure-sea-96268.herokuapp.com/api',
   TOKEN_KEY: 'auth-token-goes-here',
   USERNAME: 'current-user'
 }
